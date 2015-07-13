bfa2f8359906823e13a7c5ce6c591e2f655d359d
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2015-07-13 22:42:49 +0900

    Update Vagrant setting

--- lib/Vagrant/modules/dotfiles.sh
+++ lib/Vagrant/modules/dotfiles.sh
@@ -9,32 +9,33 @@ log=/var/log/vagrantfile.log
 home=/home/vagrant
 #}}}
 echo "dotfiles setting..."
-mkdir -p $home/work $home/.config/htop                                                                           && \
-git clone https://github.com/KazuakiM/dotfiles.git $home/work/dotfiles                          >> $log 2>> $log && \
-ln -sf $home/work/dotfiles/.bash_logout         $home/.bash_logout                                               && \
-ln -sf $home/work/dotfiles/.bash_profile        $home/.bash_profile                                              && \
-ln -sf $home/work/dotfiles/.bashrc              $home/.bashrc                                                    && \
-cp -f  $home/work/dotfiles/.bashrc.local        $home/                                                           && \
-ln -sf $home/work/dotfiles/.ctags               $home/.ctags                                                     && \
-ln -sf $home/work/dotfiles/.dir_colors          $home/.dir_colors                                                && \
-ln -sf $home/work/dotfiles/.gitconfig.org       $home/.gitconfig                                                 && \
-cp -f  $home/work/dotfiles/.gitconfig.local.org $home/.gitconfig.local                                           && \
-ln -sf $home/work/dotfiles/.gvimrc              $home/.gvimrc                                                    && \
-ln -sf $home/work/dotfiles/.htoprc              $home/.config/htop/htoprc                                        && \
-ln -sf $home/work/dotfiles/.inputrc             $home/.inputrc                                                   && \
-ln -sf $home/work/dotfiles/.mplayer             $home/.mplayer                                                   && \
-cp -f  $home/work/dotfiles/.netrc.org           $home/.netrc                                                     && \
-ln -sf $home/work/dotfiles/.screenrc            $home/.screenrc                                                  && \
-ln -sf $home/work/dotfiles/.tmux.conf           $home/.tmux.conf                                                 && \
-ln -sf $home/work/dotfiles/.vim                 $home/.vim                                                       && \
-ln -sf $home/work/dotfiles/.vimrc               $home/.vimrc                                                     && \
-cp -f  $home/work/dotfiles/.vimrc.local         $home/                                                           && \
-ln -sf $home/work/dotfiles/.w3m                 $home/.w3m                                                       && \
-git clone https://github.com/Shougo/neobundle.vim.git $home/.vim/bundle/neobundle.vim           >> $log 2>> $log && \
-wget http://jp2.php.net/distributions/manual/php_manual_ja.tar.gz -O /tmp/php_manual_ja.tar.gz  >> $log 2>> $log && \
-tar zxvf /tmp/php_manual_ja.tar.gz -C $home/.vim/vim-ref                                        >> $log 2>> $log && \
-wget http://cs.sensiolabs.org/get/php-cs-fixer.phar -O $home/.vim/vim-php-cs-fixer/php-cs-fixer >> $log 2>> $log && \
-chmod a+x $home/.vim/vim-php-cs-fixer/php-cs-fixer                                                               && \
-php $home/work/dotfiles/src/phpDict.php                                                                          && \
-sh $home/work/dotfiles/src/htmlReference.sh                                                     >> $log 2>> $log
+mkdir -p $home/work $home/.config/htop                                                                                   && \
+git clone https://github.com/KazuakiM/dotfiles.git $home/work/dotfiles                                  >> $log 2>> $log && \
+ln -sf $home/work/dotfiles/.bash_logout         $home/.bash_logout                                                       && \
+ln -sf $home/work/dotfiles/.bash_profile        $home/.bash_profile                                                      && \
+ln -sf $home/work/dotfiles/.bashrc              $home/.bashrc                                                            && \
+cp -f  $home/work/dotfiles/.bashrc.local        $home/                                                                   && \
+ln -sf $home/work/dotfiles/.ctags               $home/.ctags                                                             && \
+ln -sf $home/work/dotfiles/.dir_colors          $home/.dir_colors                                                        && \
+ln -sf $home/work/dotfiles/.gitconfig.org       $home/.gitconfig                                                         && \
+cp -f  $home/work/dotfiles/.gitconfig.local.org $home/.gitconfig.local                                                   && \
+ln -sf $home/work/dotfiles/.gvimrc              $home/.gvimrc                                                            && \
+ln -sf $home/work/dotfiles/.htoprc              $home/.config/htop/htoprc                                                && \
+ln -sf $home/work/dotfiles/.inputrc             $home/.inputrc                                                           && \
+ln -sf $home/work/dotfiles/.mplayer             $home/.mplayer                                                           && \
+cp -f  $home/work/dotfiles/.netrc.org           $home/.netrc                                                             && \
+ln -sf $home/work/dotfiles/.screenrc            $home/.screenrc                                                          && \
+ln -sf $home/work/dotfiles/.tmux.conf           $home/.tmux.conf                                                         && \
+ln -sf $home/work/dotfiles/.vim                 $home/.vim                                                               && \
+ln -sf $home/work/dotfiles/.vimrc               $home/.vimrc                                                             && \
+cp -f  $home/work/dotfiles/.vimrc.local         $home/                                                                   && \
+ln -sf $home/work/dotfiles/.w3m                 $home/.w3m                                                               && \
+git clone https://github.com/Shougo/neobundle.vim.git $home/.vim/bundle/neobundle.vim                   >> $log 2>> $log && \
+vim -N -u $home/.vimrc -c 'try | NeoBundleUpdate! | finally | qall! | endtry' -U NONE -i NONE -V1 -e -s >> $log 2>> $log && \
+wget http://jp2.php.net/distributions/manual/php_manual_ja.tar.gz -O /tmp/php_manual_ja.tar.gz          >> $log 2>> $log && \
+tar zxvf /tmp/php_manual_ja.tar.gz -C $home/.vim/vim-ref                                                >> $log 2>> $log && \
+wget http://cs.sensiolabs.org/get/php-cs-fixer.phar -O $home/.vim/vim-php-cs-fixer/php-cs-fixer         >> $log 2>> $log && \
+chmod a+x $home/.vim/vim-php-cs-fixer/php-cs-fixer                                                                       && \
+php $home/work/dotfiles/src/phpDict.php                                                                                  && \
+sh $home/work/dotfiles/src/htmlReference.sh                                                             >> $log 2>> $log
 
--- lib/Vagrant/modules/yum.sh
+++ lib/Vagrant/modules/yum.sh
@@ -8,23 +8,21 @@
 log=/var/log/vagrantfile.log
 #}}}
 echo "yum install ..."
-rpm --import http://ftp.riken.jp/Linux/fedora/epel/RPM-GPG-KEY-EPEL-7                          >> $log && \
-rpm -ivh http://ftp.riken.jp/Linux/fedora/epel/7/x86_64/e/epel-release-7-5.noarch.rpm          >> $log && \
-rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi                                    >> $log && \
-rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm                           >> $log && \
-rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt                                              >> $log && \
-rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm >> $log && \
-rpm --import http://repo.mysql.com/RPM-GPG-KEY-mysql                                           >> $log && \
-rpm -ivh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm                     >> $log && \
-yum clean all                                                                                  >> $log && \
-yum -y --enablerepo=epel,mysql,remi,rpmforge update                                            >> $log && \
-yum clean all                                                                                  >> $log && \
-yum -y --enablerepo=epel,mysql,remi,rpmforge install \
+rpm --import http://ftp.riken.jp/Linux/fedora/epel/RPM-GPG-KEY-EPEL-7                          >> $log 2>> $log && \
+rpm -ivh http://ftp.riken.jp/Linux/fedora/epel/7/x86_64/e/epel-release-7-5.noarch.rpm          >> $log 2>> $log && \
+rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi                                    >> $log 2>> $log && \
+rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm                           >> $log 2>> $log && \
+rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt                                              >> $log 2>> $log && \
+rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm >> $log 2>> $log && \
+yum clean all                                                                                  >> $log 2>> $log && \
+yum -y --enablerepo=epel,remi,rpmforge update                                                  >> $log 2>> $log && \
+yum clean all                                                                                  >> $log 2>> $log && \
+yum -y --enablerepo=epel,remi,rpmforge install \
     colordiff           ctags \
     gcc                 gcc-c++        git \
-    htop                httpd \
+    htop \
     libcurl-devel       libffi-devel   libxml2-devel         libxslt-devel          libyaml-devel lynx \
-    make                man-pages-ja   mysql-community-devel mysql-community-server \
+    make                man-pages-ja \
     ncurses-devel       nmap-ncat \
     openssl             openssl-devel \
     perl                perl-core      perl-devel            php                    php-devel     pv python python-devel \
@@ -33,5 +31,4 @@ yum -y --enablerepo=epel,mysql,remi,rpmforge install \
     the_silver_searcher tmux           tree \
     wget \
     yum-cron \
-    zlib-devel                                                                                 >> $log
-
+    zlib-devel                                                                                 >> $log 2>> $log
