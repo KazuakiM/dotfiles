e13ee4f5d65d644c93e511743ec7141088bcb901
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2015-07-22 07:50:40 +0900

    漏れ

--- lib/Vagrant/modules/luajit.sh
+++ lib/Vagrant/modules/luajit.sh
@@ -8,6 +8,8 @@
 log=/var/log/vagrantfile.log
 #}}}
 echo "LuaJIT install ..."
+yum -y --enablerepo=epel,remi,rpmforge install \
+    lua lua-devel                                                    >> $log 2>> $log && \
 git clone http://luajit.org/git/luajit-2.0.git /usr/local/src/luajit >> $log 2>> $log && \
 cd /usr/local/src/luajit/                                                             && \
 make                                                                 >> $log 2>> $log && \
