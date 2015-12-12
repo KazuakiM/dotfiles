5f55495a647b00d2a3682fa9ae7e23cb1ce3aade
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2015-12-13 06:19:11 +0900

    Add apt-cyg.sh

---
+++ lib/Cygwin/aptCyg.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#--------------------------------
+# aptGet.sh
+#
+# @author KazuakiM
+#--------------------------------
+apt-cyg update && \
+apt-cyg install gcc-core gcc-g++ git \
+     make
+
