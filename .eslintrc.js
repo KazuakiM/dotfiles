e408e96029306921817b01343fcabb2756758320
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2016-03-20 09:01:32 +0900

    Update ESLint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,8 +14,9 @@ module.exports = {
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true}},
-    "plugins": [
-        "react"],
+    //MEMO:I don't use now.
+    //"plugins": [
+    //    "react"],
     "rules": {
         //Possible Errors {{{
         "comma-dangle":   [2, "never"],
@@ -117,56 +118,27 @@ module.exports = {
         "strict": [2, "global"],
         //}}}
         //Variables {{{
-        //
-        //TODO:now writing.
-        //
+        // init-declarations (es6)
+        "no-catch-shadow": 2,
+        "no-delete-var":   2,
+        "no-label-var":    2,
+        // no-restricted-globals
+        // no-shadow (es6)
+        "no-shadow-restricted-names": 2,
+        //XXX:no-undef
+        // no-undef-init (es6)
+        "no-undefined": 2,
+        // no-unused-vars
+        // no-use-before-define (es6)
         //}}}
         //Node.js and CommonJS {{{
+        //MEMO: If I get "JavaScript Power", I set this section.
         //}}}
         //Stylistic Issues {{{
         "array-bracket-spacing": [2, "never"],
         "block-spacing":         [2, "never"],
         "brace-style":           2,
-        "jsx-quotes":            [1, "prefer-double"],
-        //}}}
-        //eslint-plugin-react {{{
-        "react/display-name":                 1,
-        "react/forbid-prop-types":            1,
-        "react/jsx-boolean-value":            1,
-        "react/jsx-closing-bracket-location": 1,
-        "react/jsx-curly-spacing":            1,
-        "react/jsx-equals-spacing":           1,
-        "react/jsx-handler-names":            1,
-        "react/jsx-indent-props":             1,
-        "react/jsx-indent":                   1,
-        "react/jsx-key":                      1,
-        "react/jsx-max-props-per-line":       1,
-        "react/jsx-no-bind":                  1,
-        "react/jsx-no-duplicate-props":       1,
-        "react/jsx-no-literals":              1,
-        "react/jsx-no-undef":                 1,
-        "react/jsx-pascal-case":              1,
-        "react/jsx-sort-prop-types":          1,
-        "react/jsx-sort-props":               1,
-        "react/jsx-uses-react":               1,
-        "react/jsx-uses-vars":                1,
-        "react/no-danger":                    1,
-        "react/no-deprecated":                1,
-        "react/no-did-mount-set-state":       1,
-        "react/no-did-update-set-state":      1,
-        "react/no-direct-mutation-state":     1,
-        "react/no-is-mounted":                1,
-        "react/no-multi-comp":                1,
-        "react/no-set-state":                 1,
-        "react/no-string-refs":               1,
-        "react/no-unknown-property":          1,
-        "react/prefer-es6-class":             1,
-        "react/prop-types":                   1,
-        "react/react-in-jsx-scope":           1,
-        "react/require-extension":            1,
-        "react/self-closing-comp":            1,
-        "react/sort-comp":                    1,
-        "react/wrap-multilines":              1
+        "jsx-quotes":            [1, "prefer-double"]
         //}}}
     }
 };
