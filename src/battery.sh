0505727b6e8bc06f6d9a051a1b6152251d1083e7
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2016-05-06 06:22:15 +0900

    Update tmux setting

---
+++ src/battery.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#--------------------------------
+# tmux-battery
+#
+# @author kazuakiM
+#--------------------------------
+battery=`pmset -g ps|egrep -o '[0-9]{2}%'|egrep -o '[0-9]{2}'|tr -d '\n'`
+if [ "$battery" -lt 20 ]; then
+    if [ "`pmset -g ps|egrep -o 'discharging'|tr -d '\n'`" = 'discharging' ]; then
+        echo "#[fg=colour234,bg=colour1,bold] $battery%"
+    else
+        echo "#[fg=colour234,bg=colour166,bold] $battery%"
+    fi
+else
+    echo "#[fg=colour234,bg=colour2,bold] $battery%"
+fi
