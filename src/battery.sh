fc3e26ac5932b5a65e4700c0fbafde3550a223e0
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2016-05-06 06:39:48 +0900

    BugFix:battery error.

--- src/battery.sh
+++ src/battery.sh
@@ -4,7 +4,7 @@
 #
 # @author kazuakiM
 #--------------------------------
-battery=`pmset -g ps|egrep -o '[0-9]{2}%'|egrep -o '[0-9]{2}'|tr -d '\n'`
+battery=`pmset -g ps|egrep -o '[0-9]{1,3}%'|egrep -o '[0-9]{1,3}'|tr -d '\n'`
 if [ "$battery" -lt 20 ]; then
     if [ "`pmset -g ps|egrep -o 'discharging'|tr -d '\n'`" = 'discharging' ]; then
         echo "#[fg=colour234,bg=colour1,bold] $battery%"
