3173a394eb97d0ff23168395d8b85c0345820735
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2017-01-21 14:39:31 +0900

    Update Vagrantfile for ubutu

--- lib/UbuntuVagrant/modules/php.sh
+++ lib/UbuntuVagrant/modules/php.sh
@@ -8,5 +8,5 @@
 log=/var/log/vagrantfile.log
 #}}}
 echo "PHP setting ..."
-apt-get install -yqq --allow-unauthenticated php7.0 php7.0-fpm php-ssh2 php7.0-zip >> $log 2>> $log && \
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin   >> $log 2>> $log
+apt-get install -yqq --allow-unauthenticated php7.0 php7.0-fpm php7.0-mbstring php7.0-soap php7.0-ssh2 php7.0-xdebug php7.0-xml php7.0-zip >> $log 2>> $log && \
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin >> $log 2>> $log
