c4237f458800b59f95949d228d7917ff6c9535c7
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2017-03-17 22:26:35 +0900

    Update battery setting

--- src/battery.sh
+++ src/battery.sh
@@ -8,6 +8,7 @@ battery=`pmset -g ps|egrep -o '[0-9]{1,3}%'|egrep -o '[0-9]{1,3}'|tr -d '\n'`
 if [ "$battery" -lt 20 ]; then
     if [ "`pmset -g ps|egrep -o 'discharging'|tr -d '\n'`" = 'discharging' ]; then
         echo "#[fg=colour234,bg=colour1,bold] $battery%"
+        growlnotify --message Battery!
     else
         echo "#[fg=colour234,bg=colour166,bold] $battery%"
     fi
