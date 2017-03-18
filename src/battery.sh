ba0b53c77a6c55e6a1c487d64a482fd7e1826490
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2017-03-18 15:36:18 +0900

    You can say

--- src/battery.sh
+++ src/battery.sh
@@ -9,6 +9,7 @@ if [ "$battery" -lt 20 ]; then
     if [ "`pmset -g ps|egrep -o 'discharging'|tr -d '\n'`" = 'discharging' ]; then
         echo "#[fg=colour234,bg=colour1,bold] $battery%"
         growlnotify --message Battery!
+        say 'マックの充電がきれます!'
     else
         echo "#[fg=colour234,bg=colour166,bold] $battery%"
     fi
