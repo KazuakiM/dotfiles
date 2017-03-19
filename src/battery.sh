821752697f72b26dd35a711f57ded264ec4d3aea
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2017-03-19 09:09:04 +0900

    Battery with growlnotify

--- src/battery.sh
+++ src/battery.sh
@@ -5,14 +5,18 @@
 # @author kazuakiM
 #--------------------------------
 battery=`pmset -g ps|egrep -o '[0-9]{1,3}%'|egrep -o '[0-9]{1,3}'|tr -d '\n'`
+charge=`pmset -g ps|egrep -o 'discharging'|tr -d '\n'`
+
 if [ "$battery" -lt 20 ]; then
-    if [ "`pmset -g ps|egrep -o 'discharging'|tr -d '\n'`" = 'discharging' ]; then
+    if [ "$charge" = 'discharging' ]; then
         echo "#[fg=colour234,bg=colour1,bold] $battery%"
-        growlnotify --message Battery!
-        say 'マックの充電がきれます!'
     else
         echo "#[fg=colour234,bg=colour166,bold] $battery%"
     fi
 else
     echo "#[fg=colour234,bg=colour2,bold] $battery%"
+fi
+
+if [ "$charge" = 'discharging' -a "$battery" -lt 35 ]; then
+    growlnotify --name Battery!
 fi
