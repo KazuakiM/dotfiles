0a1d434e4826b629efa96c930a4bd3d1b78679e2
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2017-04-25 21:04:42 +0900

    Update Vagrant setting file

--- lib/UbuntuVagrant/modules/php.sh
+++ lib/UbuntuVagrant/modules/php.sh
@@ -8,7 +8,8 @@ echo "PHP setting ..."
 add-apt-repository ppa:mapnik/boost && \
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449 && \
 echo deb http://dl.hhvm.com/ubuntu precise main | tee /etc/apt/sources.list.d/hhvm.list && \
-apt install --allow-unauthenticated php7.1 php7.1-curl php7.1-fpm php7.1-mbstring php7.1-soap php7.1-ssh2 php7.1-xdebug php7.1-xml php7.1-zip \
+apt install --allow-unauthenticated apache2 \
+    php7.1 php7.1-curl php7.1-fpm php7.1-mbstring php7.1-soap php7.1-ssh2 php7.1-xdebug php7.1-xml php7.1-zip \
     hhvm -y && \
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin && \
 echo 'xdebug.enable = 1' >> /etc/hhvm/php.ini
