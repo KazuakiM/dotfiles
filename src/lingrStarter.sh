932de3edb2f5b49810c6113eb30307957c2bcde0
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2017-06-09 22:17:34 +0900

    Update MacBook Pro (7/n)

--- src/lingrStarter.sh
+++ src/lingrStarter.sh
@@ -4,14 +4,9 @@
 #
 # @author kazuakiM
 #--------------------------------
-logFile="/Trashes/#vim-jp_reading-vimrc/`date +%Y-%m-%d`_Gitter.txt"
-
-# Log file make
-touch $logFile
 # LimeChat Start
-open -a /Users/mabuchikazuaki/Applications/LimeChat.app
+open -a LimeChat
 # GateWay Server Satrt
 nohup ruby $HOME/.vim/dein.vim/repos/github.com/psychs/lingr-irc/lig.rb > /dev/null 2>&1 &
-nohup tail -F $logFile|perl -e 'system "growlnotify", "-m",  $_ while(<STDIN>);' &
 # Finish
 exit 0
