db334b53dda4fcd2c1d3a3400f7c9fc167df7995
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2017-07-03 07:35:27 +0900

    Add Redis launchctl

--- src/launchctl.sh
+++ src/launchctl.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #--------------------------------
-# launchctlMemcached.sh
+# launchctl.sh
 #
 # @author kazuakiM
 #--------------------------------
 # Application Variable {{{
-helpMessage="\n\n\t\tLaunch memcached shell help\nParam1(required): start,stop,status\n"
+helpMessage="\n\n\t\tLaunch Memcached/Redis shell help\nParam1(required): start,stop,status\n"
 #}}}
 # Application Argument {{{
 if [ $# -eq 1 ]; then
@@ -18,12 +18,15 @@ fi
 case ${action} in
     'start')
         launchctl load $BREW_PREFIX/opt/memcached/homebrew.mxcl.memcached.plist
+        launchctl load $BREW_PREFIX/opt/redis/homebrew.mxcl.redis.plist
         ;;
     'stop')
         launchctl unload $BREW_PREFIX/opt/memcached/homebrew.mxcl.memcached.plist
+        launchctl unload $BREW_PREFIX/opt/redis/homebrew.mxcl.redis.plist
         ;;
     'status')
         ps aux|grep opt/memcached|grep -v grep
+        ps aux|grep opt/redis|grep redis|grep -v grep
         ;;
     *)
         echo ${helpMessage}
