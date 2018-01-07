80ac1bf7df8177524245dcf930e5877ee451c9fa
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2018-01-07 17:23:48 +0900

    Drop dep . add shell

---
+++ .config/go/import.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#--------------------------------
+# Go setting
+#
+# @author soramugi-San
+#--------------------------------
+go get -u github.com/nsf/gocode
+go get -u github.com/alecthomas/gometalinter
+echo "Execute: GoInstallBinariesc\n"
--- .config/go/src/github.com/dotfiles/import.go
+++
@@ -1,6 +0,0 @@
-package main
-
-import (
-	"github.com/alecthomas/gometalinter"
-	"github.com/nsf/gocode"
-)
