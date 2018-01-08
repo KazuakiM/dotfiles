c0f53e808e50714319675934b627bdf5482eb95b
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2018-01-08 22:30:55 +0900

    BugFix: golang tools

--- .config/go/import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#--------------------------------
-# Go setting
-#
-# @author soramugi-San
-#--------------------------------
-go get -u github.com/nsf/gocode
-go get -u github.com/alecthomas/gometalinter
-echo "Execute: GoInstallBinariesc\n"
